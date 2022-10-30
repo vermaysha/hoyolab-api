87d321f3f53c8bc42cefcbf381ae0736528c4c80
Ashary Vermaysha <asharyver13@gmail.com>
2022-10-30 11:28:03 +0700

    create examples

--- examples/genshin.ts
+++ examples/genshin.ts
@@ -1,9 +1,8 @@
 import { GenshinImpact } from '../src'
 
 const client = new GenshinImpact({
-  cookie:
-    'cookie_token=Cr3wjVouTTANV4Ejg7KTxxHjlI6ifXZGJ0AdgEtK; account_id=122516750; ltoken=ojsMP2EsA0ufgDo9DN4kkCBwHcCfq8Gcvadpm4Td; ltuid=122516750; mi18nLang=en-us; _MHYUUID=b1f7b5c7-6019-45f6-8fd6-1f8343f52b87; DEVICEFP_SEED_ID=7da59e6eebc0c2b4; DEVICEFP_SEED_TIME=1666483337177; G_ENABLED_IDPS=google; DEVICEFP=38d7eaf59371e',
-  uid: 826625902,
+  cookie: 'YOUR COOKIE',
+  uid: 'YOUR UID',
 })
 
 ;(async () => {
--- examples/hoyolab.ts
+++ examples/hoyolab.ts
@@ -1,8 +1,7 @@
 import { Hoyolab } from '../src'
 
 const client = new Hoyolab({
-  cookie:
-    'cookie_token=Cr3wjVouTTANV4Ejg7KTxxHjlI6ifXZGJ0AdgEtK; account_id=122516750; ltoken=ojsMP2EsA0ufgDo9DN4kkCBwHcCfq8Gcvadpm4Td; ltuid=122516750; mi18nLang=en-us; _MHYUUID=b1f7b5c7-6019-45f6-8fd6-1f8343f52b87; DEVICEFP_SEED_ID=7da59e6eebc0c2b4; DEVICEFP_SEED_TIME=1666483337177; G_ENABLED_IDPS=google; DEVICEFP=38d7eaf59371e',
+  cookie: 'YOUR COOKIE',
 })
 
 ;(async () => {
