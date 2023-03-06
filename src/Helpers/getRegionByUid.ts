ff770ce137bc25c73fff40d5eec97ffda0409587
Ashary Vermaysha <34608589+vermaysha@users.noreply.github.com>
2023-03-06 08:36:16 +0700

    Change case order & trim uid

--- src/Helpers/getRegionByUid.ts
+++ src/Helpers/getRegionByUid.ts
@@ -6,17 +6,18 @@
  * @throws {Error} if give invalid UID
  */
 export function getRegionByUid(uid: string | number): string {
-  const server_region = Number(uid.toString().slice(0, 1))
+  const server_region = Number(uid.toString().trim().slice(0, 1))
+
   switch (server_region) {
-    case 8:
-      return 'os_asia'
-    case 9:
-      return 'os_cht'
     case 7:
       return 'os_euro'
     case 6:
       return 'os_usa'
+    case 8:
+      return 'os_asia'
+    case 9:
+      return 'os_cht'
     default:
-      throw new Error('Invalid UID')
+      throw new Error(`UID ${uid} is invalid !`)
   }
 }
