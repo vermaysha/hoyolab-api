c8f78cb2e28a1cc4a183231259a34be1c6dd5852
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-10 16:04:56 +0700

    Add DynamicSecurity class for generating security tokens

---
+++ src/Utils/DynamicSecurity.ts
@@ -0,0 +1,22 @@
+import md5 from 'md5'
+
+export class DynamicSecurity {
+  static SALT = '6cqshh5dhw73bzxn20oexa9k516chk7s'
+
+  static generate(): string {
+    const date = new Date()
+    const t = Math.floor(date.getTime() / 1000)
+
+    let r = ''
+    const char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for (let i = 0; i < 6; i++) {
+      r += char.charAt(Math.floor(Math.random() * char.length))
+    }
+
+    const h = md5(`salt=${this.SALT}&t=${t}&r=${r}`, {
+      encoding: 'string',
+    })
+
+    return `${t},${r},${h}`
+  }
+}
