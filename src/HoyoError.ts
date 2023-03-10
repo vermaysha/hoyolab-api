323bb231aa23c3150971f40c1ee9621a507f6de0
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-10 20:59:04 +0700

    Add 'message' property to HoyoError class

--- src/HoyoError.ts
+++ src/HoyoError.ts
@@ -5,6 +5,7 @@
 export class HoyoError extends Error {
   readonly retmessage: string | null
   readonly retcode: number | null
+  readonly message: string
 
   constructor(
     message: string,
@@ -14,6 +15,7 @@ export class HoyoError extends Error {
     }
   ) {
     super(message)
+    this.message = message
     this.retcode = response?.retcode ?? null
     this.retmessage = response?.message ?? null
 
