ed38a56235a6302a5af9e98e8cdecb449d669e4d
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-10 16:04:27 +0700

    Create Data interface for Params, Body, and Headers

---
+++ src/Interfaces/Request/Data.ts
@@ -0,0 +1,11 @@
+export interface Params {
+  [key: string | number]: string | number | null
+}
+
+export interface Body {
+  [key: string | number]: string | number | null
+}
+
+export interface Headers {
+  [key: string | number]: string | number | null
+}
