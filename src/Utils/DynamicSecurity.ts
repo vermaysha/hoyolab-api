7403eae76d08cce69d4bcedc57e52247330c8e3c
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-10 21:05:48 +0700

    Add documentation

--- src/Utils/DynamicSecurity.ts
+++ src/Utils/DynamicSecurity.ts
@@ -1,12 +1,24 @@
 import md5 from 'md5'
 
 /**
+ * Generate Digital Signature/Dynamic Security.
+ *
+ * Used to bypass the security of the Hoyoverse API
+ *
  * @category Private
  * @internal
  */
 export class DynamicSecurity {
+  /**
+   * Cypher Salt
+   */
   static SALT = '6cqshh5dhw73bzxn20oexa9k516chk7s'
 
+  /**
+   * Generate Digital Signature for API Requst
+   *
+   * @returns string - Generated DS
+   */
   static generate(): string {
     const date = new Date()
     const t = Math.floor(date.getTime() / 1000)
