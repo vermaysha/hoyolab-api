638fd05a8255ead16ef4363091d7c4a911cc5725
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-10 20:57:12 +0700

    Add static keyword to GameRoutes class members and export GameRoutes as constant

--- src/Utils/GameRoutes.ts
+++ src/Utils/GameRoutes.ts
@@ -1,17 +1,23 @@
+/**
+ * Game Routes
+ *
+ * Contains a set of Endpoint APIs to support processes
+ * @category Private
+ * @internal
+ */
 export class GameRoutes {
-  readonly webStaticUrl = 'https://webstatic-sea.hoyoverse.com/'
-  readonly accountUrl = 'https://api-account-os.hoyolab.com/account/auth/api'
-  readonly bbsUrl = 'https://bbs-api-os.hoyolab.com/'
-  readonly bbsReferrerUrl = 'https://www.hoyolab.com/'
-  readonly takumiUrl = 'https://api-os-takumi.hoyoverse.com/'
-  readonly infoLedgerUrl =
+  static readonly webStaticUrl = 'https://webstatic-sea.hoyoverse.com/'
+  static readonly accountUrl =
+    'https://api-account-os.hoyolab.com/account/auth/api'
+  static readonly bbsUrl = 'https://bbs-api-os.hoyolab.com/'
+  static readonly bbsReferrerUrl = 'https://www.hoyolab.com/'
+  static readonly takumiUrl = 'https://api-os-takumi.hoyoverse.com/'
+  static readonly infoLedgerUrl =
     'https://sg-hk4e-api.hoyolab.com/event/ysledgeros/month_info'
-  readonly detailLedgerUrl =
+  static readonly detailLedgerUrl =
     'https://sg-hk4e-api.hoyolab.com/event/ysledgeros/month_detail'
-  readonly rewardUrl =
+  static readonly rewardUrl =
     'https://sg-hk4e-api.hoyolab.com/event/sol?act_id=e202102251931481'
-  readonly redeemUrl =
+  static readonly redeemUrl =
     'https://sg-hk4e-api.hoyoverse.com/common/apicdkey/api/webExchangeCdkey'
 }
-
-export const Routes = new GameRoutes()
