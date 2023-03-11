946c77b8e9348858c7e1bf2e0d2cb4b0191155dd
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-11 08:36:14 +0700

    Refactor GameRoutes to include new BaseURL class and update URLs for Hoyolab

--- src/Utils/GameRoutes.ts
+++ src/Utils/GameRoutes.ts
@@ -1,3 +1,13 @@
+export class BaseURL {
+  static readonly webStaticUrl = 'https://webstatic-sea.hoyolab.com/'
+  static readonly accountUrl =
+    'https://api-account-os.hoyolab.com/account/auth/api'
+  static readonly bbsUrl = 'https://bbs-api-os.hoyolab.com/'
+  static readonly bbsReferrerUrl = 'https://www.hoyolab.com/'
+  static readonly takumiUrl = 'https://api-os-takumi.hoyolab.com/'
+  static readonly sgHke4Url = 'https://sg-hk4e-api.hoyolab.com/'
+}
+
 /**
  * Game Routes
  *
@@ -5,19 +15,9 @@
  * @category Private
  * @internal
  */
-export class GameRoutes {
-  static readonly webStaticUrl = 'https://webstatic-sea.hoyoverse.com/'
-  static readonly accountUrl =
-    'https://api-account-os.hoyolab.com/account/auth/api'
-  static readonly bbsUrl = 'https://bbs-api-os.hoyolab.com/'
-  static readonly bbsReferrerUrl = 'https://www.hoyolab.com/'
-  static readonly takumiUrl = 'https://api-os-takumi.hoyoverse.com/'
-  static readonly infoLedgerUrl =
-    'https://sg-hk4e-api.hoyolab.com/event/ysledgeros/month_info'
-  static readonly detailLedgerUrl =
-    'https://sg-hk4e-api.hoyolab.com/event/ysledgeros/month_detail'
-  static readonly rewardUrl =
-    'https://sg-hk4e-api.hoyolab.com/event/sol?act_id=e202102251931481'
-  static readonly redeemUrl =
-    'https://sg-hk4e-api.hoyoverse.com/common/apicdkey/api/webExchangeCdkey'
+export class HoyolabRoutes {
+  static readonly gamesList =
+    BaseURL.takumiUrl + '/binding/api/getUserGameRolesByCookie'
+  static readonly recordsList =
+    BaseURL.bbsUrl + '/game_record/card/wapi/getGameRecordCard'
 }
