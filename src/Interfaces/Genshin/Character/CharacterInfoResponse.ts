e6ea909163dcdcf65b56cbc7ba24f2251399ce90
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-11 14:04:48 +0700

    Create CharacterInfoResponse interface

---
+++ src/Interfaces/Genshin/Character/CharacterInfoResponse.ts
@@ -0,0 +1,12 @@
+export interface CharacterInfoResponse {
+  avatars: {
+    id: number
+    image: string
+    icon: string
+    name: string
+    element: string
+    rarity: number
+    weapon_type: number
+    weapon_type_name: string
+  }[]
+}
