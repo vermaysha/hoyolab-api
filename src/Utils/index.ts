e9d427f284b2d0d9bbc466b5ba352414ffef7081
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-12 06:26:24 +0700

    Add delay function to Utils index

--- src/Utils/index.ts
+++ src/Utils/index.ts
@@ -2,3 +2,9 @@ export * from './DynamicSecurity'
 export * from './Request'
 export * from './Routes'
 export * from './ServerRegion'
+
+export function delay(second: number): Promise<unknown> {
+  return new Promise((resolve) => {
+    setTimeout(resolve, second * 1000)
+  })
+}
