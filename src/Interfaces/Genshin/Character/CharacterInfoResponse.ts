b82e402a2f6d82c6048c5dff3c672fdc42508f87
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-11 14:36:00 +0700

    Update CharacterInfoResponse interface to support empty array of avatars

--- src/Interfaces/Genshin/Character/CharacterInfoResponse.ts
+++ src/Interfaces/Genshin/Character/CharacterInfoResponse.ts
@@ -1,12 +1,14 @@
 export interface CharacterInfoResponse {
-  avatars: {
-    id: number
-    image: string
-    icon: string
-    name: string
-    element: string
-    rarity: number
-    weapon_type: number
-    weapon_type_name: string
-  }[]
+  avatars:
+    | {
+        id: number
+        image: string
+        icon: string
+        name: string
+        element: string
+        rarity: number
+        weapon_type: number
+        weapon_type_name: string
+      }[]
+    | []
 }
