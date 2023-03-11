6a11477a5e5f30aaf1ab1836c8e5ab5b1e9e36b0
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-11 12:02:12 +0700

    Update coverageReporters to include 'json-summary'

--- jest.config.ts
+++ jest.config.ts
@@ -34,7 +34,7 @@ export default {
   coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
-  coverageReporters: ['json', 'text', 'lcov', 'clover'],
+  coverageReporters: ['json', 'text', 'lcov', 'clover', 'json-summary'],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
