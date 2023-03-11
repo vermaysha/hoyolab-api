f85b924b74d6a981af779c845c288f8a96846fb5
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-11 09:20:50 +0700

    Add DailyInfoResponse interface for Genshin

---
+++ src/Interfaces/Genshin/Daily/DailyInfoResponse.ts
@@ -0,0 +1,9 @@
+export interface DailyInfoResponse {
+  total_sign_day: number
+  today: string
+  is_sign: boolean
+  first_bind: boolean
+  is_sub: boolean
+  region: string
+  month_last_day: boolean
+}
