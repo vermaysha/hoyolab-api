341e63114623250e1afa565976376caa0d1c52ae
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-23 11:46:35 +0700

    Add Wiki class for fetching character list from Wiki API

---
+++ src/Wiki.ts
@@ -0,0 +1,63 @@
+import { Body } from './Interfaces/Request/Data'
+import type {
+  WikiCharactersResponse,
+  WikiCharacterFilter,
+} from './Interfaces/Wiki'
+import { Request } from './Utils'
+
+export class Wiki {
+  /**
+   * Request object
+   *
+   * @protected
+   */
+  protected request: Request
+
+  constructor() {
+    this.request = new Request()
+  }
+
+  /**
+   * Get list of all characters
+   *
+   * @param filter WikiCharacterFilter
+   */
+  async getCharacters(
+    filter: WikiCharacterFilter | null = null
+  ): Promise<WikiCharactersResponse> {
+    const responses: Array<WikiCharactersResponse> = []
+    let page = 1
+
+    do {
+      const body: Body = {
+        menu_id: 2,
+        use_es: true,
+        page_size: 30,
+        page_num: page,
+        filters:
+          filter && Object.keys(filter).length > 0 ? Object.values(filter) : [],
+      }
+
+      this.request.setReferer('https://wiki.hoyolab.com')
+      this.request.setBody(body)
+      this.request.withDS()
+      const res = await this.request.send(
+        'https://sg-wiki-api.hoyolab.com/hoyowiki/genshin/wapi/get_entry_page_list',
+        'post'
+      )
+
+      responses.push(res.data)
+      page++
+    } while (responses[responses.length - 1].list.length > 0)
+
+    const response = responses[0]
+    responses.forEach((item, i) => {
+      if (i < 1) return
+
+      response.list = response.list.concat(item.list)
+      response.total = item.total
+    })
+
+    return response
+  }
+}
