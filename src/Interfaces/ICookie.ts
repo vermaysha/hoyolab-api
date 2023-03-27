71cc20385e54fd581f65a0dc9f5642e76ad22eba
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-27 10:05:53 +0700

    Create ICookie interface for handling cookie data

---
+++ src/Interfaces/ICookie.ts
@@ -0,0 +1,9 @@
+import type { LanguageEnum } from './LanguageEnum'
+
+export interface ICookie {
+  ltoken: string
+  ltuid: number
+  cookieToken?: string | null
+  accountId: number
+  mi18nLang?: LanguageEnum | null
+}
