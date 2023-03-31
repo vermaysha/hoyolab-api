007c9211dbbd69a8039701329c25f2eb9309fa4f
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-31 14:16:06 +0700

    Update routes to include new Genshin endpoints

--- src/routes.ts
+++ src/routes.ts
@@ -1,2 +1,12 @@
+const GENSHIN_GAME_RECORD_URL =
+  'https://bbs-api-os.hoyolab.com/game_record/genshin/api'
+
 export const GAMES_ACCOUNT =
-  'https://api-account-os.hoyoverse.com/account/binding/api/getUserGameRolesByCookieToken'
+  'https://api-account-os.hoyolab.com/account/binding/api/getUserGameRolesByCookieToken'
+
+export const GENSHIN_GAME_RECORD_REFERER = 'https://act.hoyolab.com'
+export const GENSHIN_GAME_RECORD = `${GENSHIN_GAME_RECORD_URL}/index`
+export const GENSHIN_CHARACTERS_LIST = `${GENSHIN_GAME_RECORD_URL}/character`
+export const GENSHIN_CHARACTERS_SUMMARY = `${GENSHIN_GAME_RECORD_URL}/avatarBasicInfo`
+export const GENSHIN_SPIRAL_ABYSS = `${GENSHIN_GAME_RECORD_URL}/spiralAbyss`
+export const GENSHIN_DAILY_NOTE = `${GENSHIN_GAME_RECORD_URL}/dailyNote`
