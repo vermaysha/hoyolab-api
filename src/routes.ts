082c27bab046c7180f5183088382f7fbafe03032
Ashary Vermaysha <asharyver13@gmail.com>
2023-03-31 14:21:44 +0700

    Add GENSHIN_HKE_URL and GENSHIN_DIARY and GENSHIN_DIARY_DETAIL routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 const GENSHIN_GAME_RECORD_URL =
   'https://bbs-api-os.hoyolab.com/game_record/genshin/api'
 
+const GENSHIN_HKE_URL = 'https://sg-hk4e-api.hoyolab.com/'
+
 export const GAMES_ACCOUNT =
   'https://api-account-os.hoyolab.com/account/binding/api/getUserGameRolesByCookieToken'
 
@@ -10,3 +12,6 @@ export const GENSHIN_CHARACTERS_LIST = `${GENSHIN_GAME_RECORD_URL}/character`
 export const GENSHIN_CHARACTERS_SUMMARY = `${GENSHIN_GAME_RECORD_URL}/avatarBasicInfo`
 export const GENSHIN_SPIRAL_ABYSS = `${GENSHIN_GAME_RECORD_URL}/spiralAbyss`
 export const GENSHIN_DAILY_NOTE = `${GENSHIN_GAME_RECORD_URL}/dailyNote`
+
+export const GENSHIN_DIARY = `${GENSHIN_HKE_URL}/event/ysledgeros/month_info`
+export const GENSHIN_DIARY_DETAIL = `${GENSHIN_HKE_URL}/event/ysledgeros/month_detail`
