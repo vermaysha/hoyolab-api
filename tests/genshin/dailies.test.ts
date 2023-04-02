40adc48fb98b0bdfee2b781aefc7f736d8b41229
Ashary Vermaysha <asharyver13@gmail.com>
2023-04-02 10:11:20 +0700

    Genshin Daily test

---
+++ tests/genshin/dailies.test.ts
@@ -0,0 +1,82 @@
+import test from 'ava'
+import { genshin } from './preloader'
+import { HoyolabError } from '../../src'
+
+test('dailyInfo() return should be valid', async (t) => {
+  const client = await genshin()
+  const res = await client.dailyInfo()
+
+  t.is(typeof res.total_sign_day, 'number')
+  t.is(typeof res.today, 'string')
+  t.is(typeof res.is_sign, 'boolean')
+  t.is(typeof res.first_bind, 'boolean')
+  t.is(typeof res.is_sub, 'boolean')
+  t.is(typeof res.region, 'string')
+  t.is(typeof res.month_last_day, 'boolean')
+})
+
+test('dailyRewards() return should be valid', async (t) => {
+  const client = await genshin()
+  const res = await client.dailyRewards()
+
+  t.is(typeof res.month, 'number')
+  t.is(typeof res.resign, 'boolean')
+  t.is(typeof res.now, 'string')
+
+  res.awards.forEach((award) => {
+    t.is(typeof award.icon, 'string')
+    t.is(typeof award.name, 'string')
+    t.is(typeof award.cnt, 'number')
+  })
+})
+
+test('dailyReward() should throw error', async (t) => {
+  await t.throwsAsync(
+    async () => {
+      const client = await genshin()
+      await client.dailyReward(31)
+    },
+    {
+      instanceOf: HoyolabError,
+    },
+  )
+})
+
+test('dailyReward() return should be valid', async (t) => {
+  const client = await genshin()
+  const res = await client.dailyReward()
+
+  t.is(typeof res.month, 'number')
+  t.is(typeof res.resign, 'boolean')
+  t.is(typeof res.now, 'string')
+
+  t.is(typeof res.award.icon, 'string')
+  t.is(typeof res.award.name, 'string')
+  t.is(typeof res.award.cnt, 'number')
+})
+
+test('dailyClaim() return should be valid', async (t) => {
+  const client = await genshin()
+  const res = await client.dailyClaim()
+
+  t.is(typeof res.status, 'string')
+  t.is(typeof res.code, 'number')
+
+  if (res.reward) {
+    t.is(typeof res.reward.month, 'number')
+    t.is(typeof res.reward.resign, 'boolean')
+    t.is(typeof res.reward.now, 'string')
+
+    t.is(typeof res.reward.award.icon, 'string')
+    t.is(typeof res.reward.award.name, 'string')
+    t.is(typeof res.reward.award.cnt, 'number')
+  }
+
+  t.is(typeof res.info.total_sign_day, 'number')
+  t.is(typeof res.info.today, 'string')
+  t.is(typeof res.info.is_sign, 'boolean')
+  t.is(typeof res.info.first_bind, 'boolean')
+  t.is(typeof res.info.is_sub, 'boolean')
+  t.is(typeof res.info.region, 'string')
+  t.is(typeof res.info.month_last_day, 'boolean')
+})
