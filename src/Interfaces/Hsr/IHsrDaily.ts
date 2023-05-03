67e69223d59061781ab54d5842e54e39ddd94364
Ashary Vermaysha <asharyver13@gmail.com>
2023-05-03 10:37:46 +0700

    Fix HSR API response interface

--- src/Interfaces/Hsr/IHsrDaily.ts
+++ src/Interfaces/Hsr/IHsrDaily.ts
@@ -8,10 +8,8 @@ export interface IHsrDailyInfo {
   total_sign_day: number
   today: string
   is_sign: boolean
-  first_bind: boolean
   is_sub: boolean
   region: string
-  month_last_day: boolean
 }
 
 export interface IHsrDailyReward {
