1d19342ec926016ec43b8d7c83f2c8c19ac852e0
Ashary Vermaysha <asharyver13@gmail.com>
2023-05-04 17:55:08 +0700

    change test

--- tests/helpers.test.ts
+++ tests/helpers.test.ts
@@ -1,12 +1,14 @@
 import {
+  GenshinRegion,
   HoyolabError,
+  HsrRegion,
+  Language,
   LanguageEnum,
-  getServerRegion,
-  parseLang,
-  toCamelCase,
-  toSnakeCase,
+  getGenshinRegion,
+  getHsrRegion,
 } from '../src'
 import test from 'ava'
+import { toCamelCase, toSnakeCase } from '../src/cookie/cookie.helper'
 
 test('toCamelCase should return Camel Case string', (t) => {
   const res = toCamelCase('test_error')
@@ -20,30 +22,46 @@ test('toSnakeCase should return snake_case string', (t) => {
   t.deepEqual(res, 'test_error')
 })
 
-test('getServerRegion should throw HoyolabError', (t) => {
+test('getGenshinRegion should throw HoyolabError', (t) => {
   t.throws(
     () => {
-      getServerRegion(128_983_343)
+      getGenshinRegion(128_983_343)
     },
     { instanceOf: HoyolabError },
   )
 })
 
-test('getServerRegion return should be valid', (t) => {
-  t.deepEqual(getServerRegion(62_342_343), 'os_usa')
-  t.deepEqual(getServerRegion(72_342_343), 'os_euro')
-  t.deepEqual(getServerRegion(82_342_343), 'os_asia')
-  t.deepEqual(getServerRegion(92_342_343), 'os_cht')
+test('getGenshinRegion return should be valid', (t) => {
+  t.deepEqual(getGenshinRegion(62_342_343), GenshinRegion.USA)
+  t.deepEqual(getGenshinRegion(72_342_343), GenshinRegion.EUROPE)
+  t.deepEqual(getGenshinRegion(82_342_343), GenshinRegion.ASIA)
+  t.deepEqual(getGenshinRegion(92_342_343), GenshinRegion.CHINA_TAIWAN)
+})
+
+test('getHsrRegion should throw HoyolabError', (t) => {
+  t.throws(
+    () => {
+      getHsrRegion(128_983_343)
+    },
+    { instanceOf: HoyolabError },
+  )
+})
+
+test('getHsrRegion return should be valid', (t) => {
+  t.deepEqual(getHsrRegion(62_342_343), HsrRegion.USA)
+  t.deepEqual(getHsrRegion(72_342_343), HsrRegion.EUROPE)
+  t.deepEqual(getHsrRegion(82_342_343), HsrRegion.ASIA)
+  t.deepEqual(getHsrRegion(92_342_343), HsrRegion.CHINA_TAIWAN)
 })
 
 test('parseLang return should be Language.ENGLISH when parameter null', (t) => {
-  t.deepEqual(parseLang(), LanguageEnum.ENGLISH)
+  t.deepEqual(Language.parseLang(), LanguageEnum.ENGLISH)
 })
 
 test('parseLang return should be Language.ENGLISH when parameter is invalid', (t) => {
-  t.deepEqual(parseLang('en'), LanguageEnum.ENGLISH)
+  t.deepEqual(Language.parseLang('en'), LanguageEnum.ENGLISH)
 })
 
 test('parseLang return should be Language.ENGLISH when parameter is en-us', (t) => {
-  t.deepEqual(parseLang('en-us'), LanguageEnum.ENGLISH)
+  t.deepEqual(Language.parseLang('en-us'), LanguageEnum.ENGLISH)
 })
