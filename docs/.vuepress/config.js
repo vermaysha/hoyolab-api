c9930b76777ba12ce4a0da6a9696453d85f60b77
Ashary Vermaysha <asharyver13@gmail.com>
2023-05-05 16:02:49 +0700

    Setup Vitepress configuration

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,70 @@
+const { name, description, repository } = require('../../package.json')
+
+const repo =
+  repository.url.match(/github\.com\/([\w-]+\/[\w-]+)\.git$/)[1] ?? null
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: name,
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  dest: 'docs/dist',
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    [
+      'meta',
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+    ],
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo,
+    repoLabel: 'Contribute !',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    searchPlaceholder: 'Search...',
+    nav: [
+      {
+        text: 'API Reference',
+        link: '/api/',
+      },
+    ],
+    sidebar: {},
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+    [
+      'vuepress-plugin-typedoc',
+
+      // plugin options
+      {
+        entryPoints: ['./src/index.ts'],
+        tsconfig: './tsconfig.json',
+      },
+    ],
+  ],
+}
