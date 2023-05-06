f3c449eb213daf7e7115fcc2504cb50e4052f0a0
Ashary Vermaysha <asharyver13@gmail.com>
2023-05-06 21:09:28 +0700

    Add test helper and flatten object keys

--- tests/genshin/dailies.test.ts
+++ tests/genshin/dailies.test.ts
@@ -1,6 +1,7 @@
 import test from 'ava'
 import { genshin } from './preloader'
 import { HoyolabError } from '../../src'
+import { flattenObjectKeys } from '../helper'
 
 test('daily.info() return should be valid', async (t) => {
   const client = await genshin()
@@ -13,6 +14,23 @@ test('daily.info() return should be valid', async (t) => {
   t.is(typeof res.is_sub, 'boolean')
   t.is(typeof res.region, 'string')
   t.is(typeof res.month_last_day, 'boolean')
+  t.is(typeof res.short_sign_day, 'number')
+  t.is(typeof res.sign_cnt_missed, 'number')
+
+  t.deepEqual(
+    Object.keys(res).sort(),
+    [
+      'total_sign_day',
+      'today',
+      'is_sign',
+      'first_bind',
+      'is_sub',
+      'region',
+      'month_last_day',
+      'short_sign_day',
+      'sign_cnt_missed',
+    ].sort(),
+  )
 })
 
 test('daily.rewards() return should be valid', async (t) => {
@@ -28,6 +46,19 @@ test('daily.rewards() return should be valid', async (t) => {
     t.is(typeof award.name, 'string')
     t.is(typeof award.cnt, 'number')
   })
+
+  t.deepEqual(
+    flattenObjectKeys(res).sort(),
+    [
+      'month',
+      'resign',
+      'now',
+      'biz',
+      'awards.icon',
+      'awards.name',
+      'awards.cnt',
+    ].sort(),
+  )
 })
 
 test('daily.reward() should throw error', async (t) => {
@@ -53,6 +84,19 @@ test('daily.reward() return should be valid', async (t) => {
   t.is(typeof res.award.icon, 'string')
   t.is(typeof res.award.name, 'string')
   t.is(typeof res.award.cnt, 'number')
+
+  t.deepEqual(
+    flattenObjectKeys(res).sort(),
+    [
+      'month',
+      'resign',
+      'now',
+      'biz',
+      'award.icon',
+      'award.name',
+      'award.cnt',
+    ].sort(),
+  )
 })
 
 test('daily.claim() return should be valid', async (t) => {
@@ -79,4 +123,28 @@ test('daily.claim() return should be valid', async (t) => {
   t.is(typeof res.info.is_sub, 'boolean')
   t.is(typeof res.info.region, 'string')
   t.is(typeof res.info.month_last_day, 'boolean')
+
+  t.deepEqual(
+    flattenObjectKeys(res).sort(),
+    [
+      'code',
+      'info.first_bind',
+      'info.is_sign',
+      'info.is_sub',
+      'info.month_last_day',
+      'info.region',
+      'info.short_sign_day',
+      'info.sign_cnt_missed',
+      'info.today',
+      'info.total_sign_day',
+      'reward.award.cnt',
+      'reward.award.icon',
+      'reward.award.name',
+      'reward.biz',
+      'reward.month',
+      'reward.now',
+      'reward.resign',
+      'status',
+    ].sort(),
+  )
 })
