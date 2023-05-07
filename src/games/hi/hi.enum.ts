4497ce07259a91ca429cdb653ad0eb50628f1677
Ashary Vermaysha <asharyver13@gmail.com>
2023-05-07 11:27:38 +0700

    Add HonkaiRegion enum

---
+++ src/games/hi/hi.enum.ts
@@ -0,0 +1,11 @@
+/**
+ * An enum representing Honkai servers region.
+ */
+export enum HonkaiRegion {
+  /** United States */
+  USA = 'usa01',
+  /** Europe */
+  EUROPE = 'eur01',
+  /** Asia */
+  ASIA = 'overseas01',
+}
